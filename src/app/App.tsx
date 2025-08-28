6cd2acf933cb637fb936d6321878b3e47ac7a04c
Oleksiy Nikolaenko <aleshka.dev@gmail.com>
2025-08-28 20:50:09 +0300

    created footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,13 @@
+import { Footer } from '../widgets/footer';
 import { Header } from '../widgets/header';
 
 export const App = () => {
     return (
         <>
             <Header />
+
+            <Footer></Footer>
         </>
     );
 };
+
---
+++ src/widgets/footer/ui/Footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <footer className="flex items-center justify-center">
+            <p>
+                &copy;takedelight.
+                {new Date().getFullYear()} year.All
+                rights reserved
+            </p>
+        </footer>
+    );
+};
